7a1f5a420e10c1b4e17d60c459c7de5fc704accc
nsv1 <ni.ko@mail.ru>
2019-08-14 18:39:34 +0300

    Same change
    Change settings (use class Set), make dataframe

--- syncopcuaclient.py
+++ syncopcuaclient.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # import sys
 from influxdb import InfluxDBClient
-from datetime import datetime
+from datetime import timedelta
 from opcua import Client
-from settings import readsettings, encrypt
+from settings import Set
 import concurrent.futures
 
 
@@ -23,11 +23,13 @@ class SubHandler(object):
 #            data.monitored_item.Value.StatusCode.name
 #              )
 
-        block, measurement = nodes[node.nodeid.to_string()]
+        block, measurement = Set.ua_nodes[node.nodeid.to_string()]
 #        print(block, measurement)
         status = data.monitored_item.Value.StatusCode.name
-        timestamp = data.monitored_item.Value.SourceTimestamp.strftime(
-                "%Y-%m-%dT%H:%M:%S.%f")
+        timestamp = data.monitored_item.Value.SourceTimestamp
+        timestamp -= timedelta(hours=Set.ua_timeshift)
+        timestamp = timestamp.strftime("%Y-%m-%dT%H:%M:%S.%f")
+#        print(timestamp)
         json_body = [{
             "measurement": measurement,
             "tags": {"block": block},
@@ -37,50 +39,36 @@ class SubHandler(object):
             }]
         client_db.write_points(json_body)
 #        print(block, measurement, val, status)
-#
-#    def event_notification(self, event):
-#        print("Python: New event", event)
+
+    def event_notification(self, event):
+        print("Python: New event", event)
 
 
 if __name__ == "__main__":
 
-    settings, nodes = readsettings('settings.yaml')
-    # Set OPC UA server param
-    url = settings['opcua_server']['url']
-    ua_username = settings['opcua_server']['username']
-    ua_password = encrypt(settings['opcua_server']['password'])
-    ua_period = settings['opcua_server']['period_subsription']
-    # Set inflaxdb param
-    host = settings['influxdb']['host']
-    port = settings['influxdb']['port']
-    username = settings['influxdb']['username']
-    password = encrypt(settings['influxdb']['password'])
-    mydb = settings['influxdb']['mydb']
-#    username = settings['server_opcua']['username']
-#    psw = settings['server_opcua']['password'].encode()
-#    password = base64.b64decode(psw).decode()
-
     myvar = []
-
-    client_db = InfluxDBClient(host=host, port=port,
-                               username=username, password=password)
+    client_db = InfluxDBClient(
+            host=Set.infl_host,
+            port=Set.infl_port,
+            username=Set.infl_username,
+            password=Set.infl_password)
 
     listdb = client_db.get_list_database()
-    client_db.create_database(mydb)
-    client_db.switch_database(mydb)
+    client_db.create_database(Set.infl_mydb)
+    client_db.switch_database(Set.infl_mydb)
 
-    client_ua = Client(url)
+    client_ua = Client(Set.ua_url)
     try:
-        client_ua.set_user(ua_username)
-        client_ua.set_password(ua_password)
+        client_ua.set_user(Set.ua_username)
+        client_ua.set_password(Set.ua_password)
         client_ua.connect()
         print('Client Connected')
-        for i in nodes:
+        for i in Set.ua_nodes:
             myvar.append(client_ua.get_node(i))
         handler = SubHandler()
-        sub = client_ua.create_subscription(ua_period, handler)
+        sub = client_ua.create_subscription(Set.ua_period, handler)
         handle = sub.subscribe_data_change(myvar)
-        print('Subscribe data_change with period {} ms'.format(ua_period))
+        print('Subscribe data_change with period {} ms'.format(Set.ua_period))
 #        time.sleep(5)
         input("Press Enter to continue...")
         for i in handle:
--- syncreport.py
+++ syncreport.py
@@ -1,81 +1,76 @@
 # -*- coding: utf-8 -*-
-from influxdb import InfluxDBClient, DataFrameClient
-from datetime import datetime, timedelta, timezone
-from settings import readsettings, encrypt
+from influxdb import DataFrameClient #, InfluxDBClient
+#from datetime import datetime, timedelta, timezone
+from settings import Set
 import numpy as np
 import pandas as pd
 import os
 
 
-def check_file(path, block, date_time):
-    path += '/{}{}'.format(block, date_time.strftime('/%Y/%m/%d'))
-    file = '{}{}.txt.zip'.format(block, date_time.strftime('%Y%m%d%H'))
+def check_file(path, block, date_hour):
+    path += '/{}{}'.format(block, date_hour.strftime('/%Y/%m/%d'))
+    file = '{}{}.txt.zip'.format(block, date_hour.strftime('%Y%m%d%H'))
     return True if file in os.listdir(path) else False
 
 
-def make_file(block, date_time):
-    block = '01'
-    dt = date_time.strftime("%Y-%m-%dT%H:00:00Z")
-    query_where = "select value, quality from power, quality \
+def make_df(block, date_hour):
+    block = "01"
+    dt = date_hour.strftime("%Y-%m-%dT%H:%M:%SZ")
+    query_where = "select value, quality \
+                    from frequency, power, target_power, quality \
                     where block='{0}';".format(block, dt)
 #    query_where = "select value, quality from power, frequency \
 #                    where block='{0}' and time>'{1}'-0.5s and time<'{1}'+1h+0.5s;".format(block, dt)
 
-    result = client.query(query_where)
-#    result.raw
-#    y = result.items()
-#    y = [i for i in y[0][1]]
-#    print(y)
-    res = pd.DataFrame(index=[str(i) for i in range(3600)])
-    for i in result:
-        df = result[i]
+    dict_df = client.query(query_where)
+    result = pd.DataFrame(index=range(3600))
+    for i in dict_df:
+        df = dict_df[i]
         # округляем до секунд
-        df.index = df.index.round('S').replace(tzinfo=timezone.utc)
-
+        df.index = df.index.round('S')
         # приводим к сеундам
-        df.index = df.index - date_time.replace(tzinfo=timezone.utc)
-
+        df.index -= date_hour
+        df.index = df.index.seconds
         # группируем и находим среднее значение в группе
-        df.pivot_table(values='value', index=df.index, 
+        dfi = df.pivot_table(values='value', index=df.index, 
                        aggfunc=np.mean, fill_value=0)
-        
-        
-        grouped = df.groupby(df.index)
-        res = grouped['value'].agg(np.mean) # Результат - Series
-        res.index = res.index.strftime('%S')
-        res.to_frame() # Результат - DataFrame
-        
+        # присоединяем dfi к результату по индексам
+        result = result.join(dfi)
+        result = result.rename(columns={"value": i})
+    print(result)
+    return result
 
 def make_report():
-    now = datetime.utcnow()
+#    now = datetime.utcnow()
+    now = pd.Timestamp.now('UTC')
     # Определение текущей даты и часа со смещением часа (shift+1)
-    date_time = now - timedelta(
-            hours=1,
-            minutes=now.minute,
-            seconds=now.second,
-            microseconds=now.microsecond)
+    date_hour = now.replace(minute=0, second=0, microsecond=0, nanosecond=0)
+#    date_time = now - timedelta(
+#            hours=1,
+#            minutes=now.minute,
+#            seconds=now.second,
+#            microseconds=now.microsecond)
     for i in blocks:
-        if not check_file(path, i, date_time):
-            make_file(i, date_time)
-
+        make_df(i, date_hour)
+#        if not check_file(path, i, date_hour):
+#            make_file(i, date_hour)
+    
 
 if __name__ == "__main__":
     
-    settings, node_id_lst = readsettings('settings.yaml')
-    host = settings['influxdb']['host']
-    port = settings['influxdb']['port']
-    username = settings['influxdb']['username']
-    password = encrypt(settings['influxdb']['password'])
-    mydb = settings['influxdb']['mydb']
-    path = settings['syncreport']['filestorige']['path']
-    timeshift = settings['syncreport']['timeshift']
-    blocks = settings['opcua_server']['block']
+    mydb = Set.infl_mydb
+    path = Set.sr_path
+    timeshift = Set.sr_timeshift
+    blocks = Set.sr_blocks
 
 #    client = InfluxDBClient(host=host, port=port,
 #                            username=username, password=password)
-    client = DataFrameClient(host=host, port=port,
-                            username=username, password=password)
-
+    client = DataFrameClient(
+            host=Set.infl_host,
+            port=Set.infl_port,
+            username=Set.infl_username,
+            password=Set.infl_password)
+    
     listdb = client.get_list_database()
     listdb = [i['name'] for i in listdb]
 #    print(listdb)
